a30002659f6cce701e6d0341b6524e4a7ae97337
DK\psk <psk@sitecore.net>
2018-05-03 17:09:49 +0300

    Add code difference between 8.1.2 and 8.2.1

--- src/Sitecore.Support.140668/AnalyticsAddressAggregator.cs
+++ src/Sitecore.Support.140668/AnalyticsAddressAggregator.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using Sitecore.Analytics.Aggregation.Pipeline;
 using Sitecore.ContentSearch.Analytics.Models;
+using Sitecore.ContentSearch.Diagnostics;
+using Sitecore.ContentSearch.Analytics.Extensions;
 
 namespace Sitecore.Support.ContentSearch.Analytics.Aggregators
 {
@@ -11,7 +13,11 @@ namespace Sitecore.Support.ContentSearch.Analytics.Aggregators
     protected override IEnumerable<IAddressIndexable> ResolveIndexables(AggregationPipelineArgs args)
     {
       if (args.Context.Contact == null) return new AddressIndexable[0];
-
+      if (!args.Context.Contact.ShouldBeIndexed())
+      {
+        ObservationLog.Log.Debug(() => string.Format("The contact addresses will not be indexed because contact {0} does not have an identifier and the system is not configured to index anonymous contacts and their interactions.", args.Context.Contact.Id), null);
+        return new AddressIndexable[0];
+      }
       return AggregatorHelper.ResolveAddressIndexables(args.Context.Contact);
     }
   }
--- src/Sitecore.Support.140668/ContactChangeAddressAggregator.cs
+++ src/Sitecore.Support.140668/ContactChangeAddressAggregator.cs
@@ -2,6 +2,9 @@
 using Sitecore.Analytics.Model.Entities;
 using Sitecore.ContentSearch;
 using Sitecore.ContentSearch.Analytics.Models;
+using System.Linq;
+using Sitecore.ContentSearch.Diagnostics;
+using Sitecore.ContentSearch.Analytics.Extensions;
 
 namespace Sitecore.Support.ContentSearch.Analytics.Aggregators
 {
@@ -14,6 +17,11 @@ namespace Sitecore.Support.ContentSearch.Analytics.Aggregators
 
     protected override IEnumerable<IAddressIndexable> ResolveIndexables(IContact contact, ChangeEventReason reason)
     {
+      if (!contact.ShouldBeIndexed())
+      {
+        ObservationLog.Log.Debug(() => string.Format("The contact addresses will not be indexed because contact {0} does not have an identifier and the system is not configured to index anonymous contacts and their interactions.", contact.Id), null);
+        return Enumerable.Empty<IAddressIndexable>();
+      }
       return AggregatorHelper.ResolveAddressIndexables(contact);
     }
   }
